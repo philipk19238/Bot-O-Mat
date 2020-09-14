4da7dada74c6fb04e2d543cef74f816ba463837e
philipk19238 <pkung67@utexas.edu>
2020-09-14 02:41:44 -0500

    Updates

---
+++ precache-manifest.3b3b755bf970a2e30520c6cec14b9aa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b318440fac9622cedd4578c080b614",
+    "url": "/botomat/index.html"
+  },
+  {
+    "revision": "69e915fd430314c01117",
+    "url": "/botomat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53e33f9cbce1eee1fee5",
+    "url": "/botomat/static/js/2.ff1f6295.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/botomat/static/js/2.ff1f6295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69e915fd430314c01117",
+    "url": "/botomat/static/js/main.fc25003f.chunk.js"
+  },
+  {
+    "revision": "3b6eb2e722b280d48f71",
+    "url": "/botomat/static/js/runtime-main.4bd1c21b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/botomat/static/media/logo.5d5d9eef.svg"
+  }
+]);
